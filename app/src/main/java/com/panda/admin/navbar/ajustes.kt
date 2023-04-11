d22bd46b6195203a292b95123e0dafd194eae533
xGrogos <victoruyflores@hotmail.com>
2023-04-11 18:46:14 +0200

    push push pussshhh

--- app/src/main/java/com/panda/admin/navbar/ajustes.kt
+++ app/src/main/java/com/panda/admin/navbar/ajustes.kt
@@ -1,19 +1,32 @@
 package com.panda.admin.navbar
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
+import com.panda.admin.MainActivity
 import com.panda.admin.R
 
+
 class ajustes : Fragment() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ajustes, container, false)
+    ): View {
+        val view: View = inflater.inflate(R.layout.fragment_ajustes, container, true)
+        var buttonLogout: Button = view.findViewById(R.id.btnLogout)
+        buttonLogout.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            val intent = Intent(context, MainActivity::class.java)
+            startActivity(intent)
+            requireActivity().finish() // Close the current activity
+        }
+        return view
     }
 }
